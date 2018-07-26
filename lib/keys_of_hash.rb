960e3bdd4749cca4517f2ae91423b7d34c4cd5c5
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-26 05:29:29 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+def animals
+  animals = 
+   {"sugar glider"=>"Australia",
+   "aye-aye"=> "Madagascar",
+   "red-footed tortoise"=>"Panama",
+   "kangaroo"=> "Australia",
+   "tomato frog"=>"Madagascar",
+   "koala"=>"Australia"} 
+end 
+  
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     array = []
     self.each do |key, value|
       arguments.each do |i|
